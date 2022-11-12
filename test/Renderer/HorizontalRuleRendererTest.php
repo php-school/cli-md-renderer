67ad89d266ccce56f25b303d0ed178339997d03b
Michael Woodward <mikeymike.mw@gmail.com>
2022-11-12 00:23:43 +0000

    Add test for horizontal rule with width from env

--- test/Renderer/HorizontalRuleRendererTest.php
+++ test/Renderer/HorizontalRuleRendererTest.php
@@ -31,4 +31,20 @@ class HorizontalRuleRendererTest extends AbstractRendererTest implements Rendere
             $renderer->render($rule, $cliRenderer)
         );
     }
+
+    public function testRendererWithWidthOptionOnEnvironment(): void
+    {
+        $class = $this->getRendererClass();
+        $renderer = new $class();
+        $rule = new ThematicBreak();
+
+        $color = new Color();
+        $color->setForceStyle(true);
+        $cliRenderer = new CliRenderer(new Environment(['renderer' => ['width' => 10]]), $color);
+
+        $this->assertEquals(
+            "[90m----------[0m",
+            $renderer->render($rule, $cliRenderer)
+        );
+    }
 }
